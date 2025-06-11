b0e6df6792b92a9d0fec02ea678b510b6fbc2457
Kelvin Mensah <klvnalexis9@gmail.com>
2025-06-11 19:31:48 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,11 @@
+import gradio as gr
+from src.app.b_primary_model import chat_osagyefo
+
+demo_chatbot = gr.ChatInterface(chat_osagyefo,
+                                title="OSAGYEFO ESQ.",
+                                description="Welcome! Put forth your local legal challenges. ")
+
+
+
+if __name__ == "__main__":
+    demo_chatbot.launch()
--- main.py
+++
@@ -1,8 +0,0 @@
-import gradio as gr
-from src.app.b_primary_model import chat_osagyefo
-
-demo_chatbot = gr.ChatInterface(chat_osagyefo,
-                                title="OSAGYEFO ESQ.",
-                                description="Welcome! Put forth your legal challenges. ")
-
-demo_chatbot.launch()
