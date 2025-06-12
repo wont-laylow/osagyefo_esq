12d6a4c1b2f1d640bf5a87fb0a4f519f969d7d8a
Kelvin Mensah <klvnalexis9@gmail.com>
2025-06-12 15:50:04 +0000

    docker, and final steps

--- src/app/load_embeddings.py
+++ src/app/load_embeddings.py
@@ -12,6 +12,6 @@ class PreRag:
     )
 
     retriever = vector_store.as_retriever(
-        search_type="mmr",  # Maximal Marginal Relevance
+        search_type="mmr",  
         search_kwargs={"k": 5, "lambda_mult": 0.5}
     )
